646438333d789c11fab10dd58a9ed599d05194c3
Ildar Ibragimov <ibragimovildar229@gmail.com>
2024-04-04 21:36:12 +0300

    liquibase-and-hibernate-v2

--- hibernate-dao-liquibase/src/main/java/course/hibernatedaoliquibase/entity/Persons.java
+++ hibernate-dao-liquibase/src/main/java/course/hibernatedaoliquibase/entity/Persons.java
@@ -6,6 +6,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @ToString
+@IdClass(PersonsId.class)
 @Entity
 public class Persons {
 
---
+++ hibernate-dao-liquibase/src/main/java/course/hibernatedaoliquibase/entity/PersonsId.java
@@ -0,0 +1,17 @@
+package course.hibernatedaoliquibase.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class PersonsId implements Serializable {
+    private String name;
+    private String surName;
+    private int age;
+}
